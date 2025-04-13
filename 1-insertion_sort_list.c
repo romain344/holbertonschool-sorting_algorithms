106a8396ccdeb558b4f048c6efcc2787b56aa613
Sheynon <labadie.jonatahan@hotmail.fr>
2025-04-13 17:59:04 +0200

    fixed - betty syntaxe

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,7 +3,7 @@
 
 /**
  * insertion_sort_list - Sorts a doubly linked list of integers
- * 			 in ascending order using the Insertion sort algorithm
+ * in ascending order using the Insertion sort algorithm
  * @list: Double pointer to the head of the doubly linked list
  */
 void insertion_sort_list(listint_t **list)
