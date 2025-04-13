4a7ef20b50f07b0094c75a3a796789e8b85403a9
Sheynon <labadie.jonatahan@hotmail.fr>
2025-04-13 13:17:00 +0200

    added - function to quick sort an array, print it, and hid header file

---
+++ 3-quick_sort.c
@@ -0,0 +1,84 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * swap - function to swap value
+ * @a: pointer to the first value
+ * @b: pointer to the second value
+ */
+
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * partition - function to find the partition position
+ * @array: array to sort
+ * @low: starting index of the partition
+ * @high: ending index of the partition
+ *
+ * Return: return the partition point
+ */
+
+int partition(int array[], int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low, j;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
+
+	if (i != high)
+	{
+		swap(&array[i], &array[high]);
+		print_array(array, size);
+	}
+
+	return (i);
+}
+
+/**
+ * quick_sort_rec - Recursive quick sort
+ * @array: array to sort
+ * @low: starting index
+ * @high: ending index
+ */
+
+void quick_sort_rec(int *array, int low, int high, size_t size)
+{
+	int pivot_index;
+
+	if (low < high)
+	{
+		pivot_index = partition(array, low, high, size);
+		quick_sort_rec(array, low, pivot_index - 1, size);
+		quick_sort_rec(array, pivot_index +1, high, size);
+	}
+}
+
+/**
+ * quick_sort - function to quicksort the value of an array
+ * @array: pointer to the array
+ * @size: size of the array
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+		return;
+
+	quick_sort_rec(array, 0, size -1, size);
+}
