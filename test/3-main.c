59835ea567e80890875d4cec116e0a457e2ea63e
Sheynon <labadie.jonatahan@hotmail.fr>
2025-04-13 13:12:28 +0200

    added - test directory

---
+++ test/3-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int array[] = {1, 448, 9, 171, 123, 452, 996, 373, 860, 27, 65, 32654, 0, 65};
+    size_t n = sizeof(array) / sizeof(array[0]);
+
+    print_array(array, n);
+    printf("\n");
+    quick_sort(array, n);
+    printf("\n");
+    print_array(array, n);
+    return (0);
+}
