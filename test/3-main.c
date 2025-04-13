d67d77d58593cb08ab80f5185548a7c36825d6f8
Sheynon <labadie.jonatahan@hotmail.fr>
2025-04-13 14:56:19 +0200

    added - main.c to test

--- test/3-main.c
+++ test/3-main.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-    int array[] = {1, 448, 9, 171, 123, 452, 996, 373, 860, 27, 65, 32654, 0, 65};
+    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
     size_t n = sizeof(array) / sizeof(array[0]);
 
     print_array(array, n);
