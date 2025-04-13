bb92d0f65e62586a60cbd3808d5f31269be27611
Sheynon <labadie.jonatahan@hotmail.fr>
2025-04-13 14:01:41 +0200

    fixed - output

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -26,7 +26,7 @@ void swap(int *a, int *b)
 int Lomuto_partition(int array[], int low, int high, size_t size)
 {
 	int pivot = array[high];
-	int i = low, j;
+	int i = low - 1, j;
 
 	for (j = low; j < high; j++)
 	{
